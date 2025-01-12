dda1f6874b6f60e732aee95c9b5e5e639a5869f3
maoucodes <162734089+maoucodes@users.noreply.github.com>
2025-01-12 23:15:45 +0530

    Update auth.js

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = 'https://wccback.vercel.app/api';
 
 // Configure axios defaults
 axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
